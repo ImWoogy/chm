3d12142e325a1834ef54e51fff83cdd5f85de484
Mr.Woogy <woogy@live.ru>
2024-09-21 06:09:32 +0800

    Update ewm.js

--- ewm/ewm.js
+++ ewm/ewm.js
@@ -1,3 +1,19 @@
-function Test(){
-alert("Test EWM");
+// Функция Test, которую мы будем вызывать из основного скрипта
+function Test() {
+    console.log('Функция Test успешно вызвана из удаленного скрипта!');
+}
+
+// Можно также определить другие функции или переменные, если нужно
+function anotherFunction() {
+    console.log('Это еще одна функция из удаленного скрипта.');
+}
+
+// Экспортируем функцию Test, если модульная система поддерживается (например, CommonJS или ES6 модули)
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = {
+        Test: Test
+    };
+} else {
+    // Для браузера добавляем функцию Test в глобальный объект window
+    window.Test = Test;
 }
